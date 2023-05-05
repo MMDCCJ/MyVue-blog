e38e692123322bce1bbec97f97cfe40a66333b27
MMDCCJ <654371320@qq.com>
2023-05-05 17:06:13 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>this is a new message</p>
+    <B_Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import B_Header from './components/Blog-Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    B_Header
+  },
+  data() {
+    return {
+      radio: '1'
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
